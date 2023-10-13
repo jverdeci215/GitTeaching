677bbf3cd3511b6493f0575baa99d179caf95008
Justin <jverdeci215@gmail.com>
2023-10-13 13:41:06 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def add(x,y):
+    return x+y
+
+if __name__ == "__main__":
+    num1 = input("Please Enter A Number: ")
+    num2 = input("Please Enter Another Number: ")
+    print("The sum is: {}".format(add(int(num1),int(num2))))
